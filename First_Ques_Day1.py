1eaf87ff607162c78655494fb82a6d2d040ce172
apurkait <79083303+apurkait@users.noreply.github.com>
2021-02-23 00:49:22 +0530

    Delete First_Ques_Day1.py

--- First_Ques_Day1.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    first_num = int(input())  # Considering Integer inputs
-    second_num = int(input())
-    print(first_num + second_num)  # Addition Operation
-    print(first_num - second_num)  # Subtraction Operation
-    print(first_num * second_num)  # Multiplication Operation
-    print(first_num / second_num)  # Float Division
-    print(int(first_num // second_num))  # Integer Division
